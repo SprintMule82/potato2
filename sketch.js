ec0e34db431a2954b75c63d3eb7901fab7e99a2a
Peter Trinh <petertrinh@protonmail.com>
2018-07-12 17:26:27 -0400

    changed the color to brown

--- sketch.js
+++ sketch.js
@@ -1,18 +1,18 @@
 let size = 20;
-
 function setup() {
   createCanvas(1000, 800);
+
 }
 
 function draw() {
   if (mouseIsPressed) {
-    fill(200, 234, 5 );
+    fill(206, 181, 153);
     ellipse(mouseX, mouseY, size, size);
   }
 }
 
 function keyPressed() {
-  switch(keyCode) {
+  switch (keyCode) {
     case 32:
       background(255);
       break;
